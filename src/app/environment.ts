bcbbc2b3a46b370ec725471df86a5e939b07d83f
Bublik Hum-num <Iris_Gonzales103@mail.ru>
2024-05-25 20:07:41 +0300

    'fix/read.me

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
-  serverUrl: 'https://curriculums.kzkvv.me/api',
-  //serverUrl: 'http://localhost:3000/api',
+  serverUrl: 'http://localhost:3000/api',
 
   // serverUrl: process.env['SERVER_URL']
 };
